02a333d29e2b73936812f804f725eb50c4d74775
Valentin Berger <Valentin.berger38@gmail.com>
2020-01-28 14:53:33 +0100

    :sparkles: Add `Cardinal`

--- main/java/fr/polytech/sma/ants/Ant.kt
+++ main/java/fr/polytech/sma/ants/Ant.kt
@@ -3,28 +3,28 @@ package fr.polytech.sma.ants
 import java.util.*
 
 open class Ant(
-    private var _id: UUID = UUID.randomUUID(),
-    private var _position: Position
+	private var _id: UUID = UUID.randomUUID(),
+	private var _position: Position
 ) : Element() {
-
-    var id: UUID
-        get() = _id
-        set(value) { _id = value }
-
-    var position: Position
-        get() = _position
-        set(value) { _position = value }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Ant) return false
-
-        if (_position != other._position) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return _position.hashCode()
-    }
+	
+	var id: UUID
+		get() = _id
+		set(value) { _id = value }
+	
+	var position: Position
+		get() = _position
+		set(value) { _position = value }
+	
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (other !is Ant) return false
+		
+		if (_position != other._position) return false
+		
+		return true
+	}
+	
+	override fun hashCode(): Int {
+		return _position.hashCode()
+	}
 }
---
+++ main/java/fr/polytech/sma/ants/Cardinal.kt
@@ -0,0 +1,65 @@
+package fr.polytech.sma.ants
+
+enum class Cardinal(
+	val position: Position
+) {
+	CENTER(0, 0),
+	NORTH(0, -1),
+	EAST(1, 0),
+	SOUTH(0, 1),
+	WEST(-1, 0);
+	
+	constructor(x: Int, y: Int) : this(Position(x, y))
+	
+	fun invert(): Cardinal {
+		return when (this) {
+			CENTER -> CENTER
+			NORTH -> SOUTH
+			EAST -> WEST
+			SOUTH -> NORTH
+			WEST -> EAST
+		}
+	}
+	
+	fun turnClockwise(): Cardinal {
+		return when (this) {
+			CENTER -> CENTER
+			NORTH -> EAST
+			EAST -> SOUTH
+			SOUTH -> WEST
+			WEST -> NORTH
+		}
+	}
+	
+	fun turnAnticlockwise(): Cardinal = turnClockwise().invert()
+	
+	operator fun plus(cardinal: Cardinal): Position {
+		return this.position + cardinal.position
+	}
+	operator fun plus(position: Position): Position {
+		return this.position + position
+	}
+	
+	operator fun minus(cardinal: Cardinal): Position {
+		return this.position - cardinal.position
+	}
+	operator fun minus(position: Position): Position {
+		return this.position - position
+	}
+	
+	operator fun times(cardinal: Cardinal): Position {
+		return this.position * cardinal.position
+	}
+	operator fun times(position: Position): Position {
+		return this.position * position
+	}
+	
+	operator fun div(cardinal: Cardinal): Position {
+		return this.position/cardinal.position
+	}
+	operator fun div(position: Position): Position {
+		return this.position/position
+	}
+	
+	operator fun not(): Cardinal = invert()
+}
